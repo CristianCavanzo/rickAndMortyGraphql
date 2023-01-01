f9cb4e403ca1e3683a9966d96ae0c34fe4bd1e34
Cristian Cavanzo <cristian.frontendev@gmail.com>
2022-12-31 20:18:20 -0500

    Some

--- src/components/Layout/LayoutHome.tsx
+++ src/components/Layout/LayoutHome.tsx
@@ -74,6 +74,7 @@ const LayoutHome = ({
 									<ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
 								</div>
 							)}
+							{}
 							{/* <a
 								href="/"
 								aria-current="page"
